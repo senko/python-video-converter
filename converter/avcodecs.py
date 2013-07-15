2d053e8bc72b7c528f47694023fec9f5278a34ec
Taha Jahangir <mtjahangir@gmail.com>
2013-07-15 10:30:06 +0430

    Configurable x264 preset and crf

--- converter/avcodecs.py
+++ converter/avcodecs.py
@@ -341,18 +341,24 @@ class AacCodec(AudioCodec):
 class H264Codec(VideoCodec):
     """
     H.264/AVC video codec.
+    @see http://ffmpeg.org/trac/ffmpeg/wiki/x264EncodingGuide
     """
     codec_name = 'h264'
     ffmpeg_codec_name = 'libx264'
-
-    x264_voodoo_recipe_ipod = ("-flags +loop -cmp chroma " +
-        "-partitions +parti4x4+partp8x8+partb8x8 -subq 5 -trellis 1 " +
-        "-refs 1 -coder 0 -me_range 16 -g 300 -keyint_min 25 " +
-        "-sc_threshold 40 -i_qfactor 0.71 -rc_eq 'blurCplx^(1-qComp)' " +
-        "-qcomp 0.6 -qmin 10 -qmax 51 -qdiff 4 -level 30")
+    encoder_options = VideoCodec.encoder_options.copy()
+    encoder_options.update({
+        'preset': str,
+        'quality': int,  # constant rate factor, range:0(lossless)-51(worst)
+                         # default:23, recommended: 18-28
+    })
 
     def _codec_specific_produce_ffmpeg_list(self, safe):
-        return self.x264_voodoo_recipe_ipod.split(' ')
+        optlist = []
+        if 'preset' in safe:
+            optlist.extend(['-preset', safe['preset']])
+        if 'quality' in safe:
+            optlist.extend(['-crf', safe['quality']])
+        return optlist
 
 
 class Mp3Codec(AudioCodec):
