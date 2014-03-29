cf369304b38c8dcedc7b9eedf58c4f39e2a9217b
Omid Raha <omidraha.com@gmail.com>
2014-03-29 18:25:27 +0430

    fix ffmpeg binary link for travis (also using last version of ffmpeg)

--- test/install-ffmpeg.sh
+++ test/install-ffmpeg.sh
@@ -4,6 +4,6 @@
 # add-apt-repository -y ppa:jon-severinsson/ffmpeg
 # apt-get -y -qq update
 # apt-get -y -qq install ffmpeg
-
-wget -P /tmp http://ffmpeg.gusari.org/static/64bit/ffmpeg.static.64bit.2014-01-10.tar.gz
-tar -xvf /tmp/ffmpeg.static.64bit.2014-01-10.tar.gz -C /usr/local/bin
+# list directory of ffmpeg binary file there is in http://ffmpeg.gusari.org/static/64bit/
+wget -P /tmp http://ffmpeg.gusari.org/static/64bit/ffmpeg.static.64bit.latest.tar.gz
+tar -xvf /tmp/ffmpeg.static.64bit.latest.tar.gz -C /usr/local/bin
